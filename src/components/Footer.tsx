eb87028e2ffad8f546a5b5adf38e1fbfd2a64009
HammadMustafa07 <hammadmustafa6638@gmail.com>
2025-01-07 11:03:38 +0500

    initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,18 @@
 export default function Footer() {
     return (
         <>
-        <div className="[panel] h-[300px] w-[1349.4px] bg-[#001738] flex justify-center items-center border-b-[2px] border-gray-400">
-            <p className="text-gray-300 text-[24px]">"Empowering minds with the latest in AI and IT skills  Your journey to innovation starts here."</p>
-        </div>
-        <div className="[Footer] h-[40px] bg-[#001738] items-center flex">
-            <div className="[Icons-div] w-[40px] flex justify-evenly gap-2 ml-[250px]">
-            <a href="https://github.com/HammadMustafa07"><i className="bi bi-github text-[24px] text-white"></i></a>
-            <a href="https://www.linkedin.com/in/hammad-mustafa-b0462a338/"><i className="bi bi-linkedin text-[24px] text-white"></i></a>
-            <a href="https://www.instagram.com/hammadabro_x/"><i className="bi bi-instagram text-[24px] text-white"></i></a>
+            <div className="[panel] h-[300px] w-[1349.4px] bg-[#001738] flex justify-center items-center border-b-[2px] border-gray-400">
+                <p className="text-gray-300 text-[24px]">&quot;Empowering minds with the latest in AI and IT skills. Your journey to innovation starts here.&quot;
+                </p>
             </div>
-            <div className="ml-[500px] text-white">© 2025 Hammad Mustafa. All Rights Reserved.</div>
-        </div>
+            <div className="[Footer] h-[40px] bg-[#001738] items-center flex">
+                <div className="[Icons-div] w-[40px] flex justify-evenly gap-2 ml-[250px]">
+                    <a href="https://github.com/HammadMustafa07"><i className="bi bi-github text-[24px] text-white"></i></a>
+                    <a href="https://www.linkedin.com/in/hammad-mustafa-b0462a338/"><i className="bi bi-linkedin text-[24px] text-white"></i></a>
+                    <a href="https://www.instagram.com/hammadabro_x/"><i className="bi bi-instagram text-[24px] text-white"></i></a>
+                </div>
+                <div className="ml-[500px] text-white">&copy; 2025 Hammad Mustafa. All Rights Reserved.</div>
+            </div>
         </>
     );
 }
